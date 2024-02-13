e9af82d26df9c6d2265c4898018de9100483677a
Dmytro <dimon.pupkin.96@gmail.com>
2024-02-13 21:59:57 +0200

    Task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-cherker';
+
+  strength:string = "";
+  
+  handleInputChange(event: Event): void {
+    const inputValue = (event.target as HTMLInputElement).value;
+
+    this.checkPasswordStrength(inputValue.replaceAll(" ", ""));
+  }
+
+  checkPasswordStrength(password:string) {
+
+    if (password.length == 0){
+      this.strength = "gray";
+    } else if (password.length < 8) {
+      this.strength = "very weak";
+    } else if (password.match(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&*!])[a-zA-Z\d@#$%^&*!]+$/) ){
+      this.strength = "strong";
+    } else if (
+      password.match(/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$/) 
+    || password.match(/^(?=.*[a-zA-Z])(?=.*[@#$%^&*!])[a-zA-Z\d@#$%^&*!]+$/) 
+    || password.match(/^(?=.*\0-9)(?=.*[@#$%^&*!])[a-zA-Z\d@#$%^&*!]+$/)) 
+    {
+      this.strength = "medium";
+    } else if (
+      password.match(/^[a-zA-Z]+$/) 
+    || password.match(/^[\d]+$/) 
+    || password.match(/^[!@#$%^&*(),.?":{}|<>]+$/))
+    {
+      console.log("weak")
+      this.strength = "weak";
+    }
+  }
 }
